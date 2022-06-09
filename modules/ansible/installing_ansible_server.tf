63b083ad932003fdd6109611828e1481829ccc36
lihilu <lihi.reisman@gmail.com>
2022-06-09 09:30:47 +0300

    ansible cron

--- modules/ansible/installing_ansible_server.tf
+++ modules/ansible/installing_ansible_server.tf
@@ -21,7 +21,12 @@ sleep 9m
 git clone https://github.com/lihilu/kandula_ansible.git /home/ubuntu/kandula_project/ && echo "cloned"
 sleep 1m
 
+
+echo "Creating cron..."
+sudo sh -c 'echo "#!/bin/bash \nsudo ansible-playbook -i /home/ubuntu/kandula_project/ansible/inventory_aws_ec2.yml /home/ubuntu/kandula_project/ansible/playbook_consul.yml" > /etc/cron.hourly/playbookrunforconsol.sh'
+sudo chmod +x /etc/cron.hourly/playbookrunforconsol.sh
 sudo mv /home/ubuntu/kandula_project/ansible/ansible.cfg /etc/ansible/ansible.cfg
 ansible-playbook -i /home/ubuntu/kandula_project/ansible/inventory_aws_ec2.yml /home/ubuntu/kandula_project/ansible/playbook_consul.yml
+
 USERDATA
 }
